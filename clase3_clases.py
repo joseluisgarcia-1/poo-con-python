9fd6d7f5c083d60990850507c7d358a873989343
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2022-04-25 17:12:33 -0500

    apuntes hasta la clase 3 media clase

--- clase3_clases.py
+++ clase3_clases.py
@@ -4,11 +4,11 @@ class Persona:
         self.edad = edad
     
     def saluda(self, otra_persona):
-        return f'Hola {otra_persona.nombre} mi nombre es {self.nombre}'
+        print('Hola',otra_persona.nombre, 'mi nombre es:',self.nombre)
         #la línea de abajo se usa con python3.6 en adelante
         #return f'Hola {otra_persona.nombre}, mi nombre es: {self.nombre}.'
 
-messi = Persona('Lio', 34)
+messi = Persona("Lio", 34)
 cr = Persona("Cr", 37)
 #invocando la función saluda
-messi.saluda("mbappe")
+messi.saluda(cr)
