5db1042735c67948f1a22672e8fb29cbae8c1a1f
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2022-04-26 09:19:14 -0500

    apuntes completos hasta la clase 3 de instancias y clases

--- clase3_clases.py
+++ clase3_clases.py
@@ -5,10 +5,11 @@ class Persona:
     
     def saluda(self, otra_persona):
         print('Hola',otra_persona.nombre, 'mi nombre es:',self.nombre)
+        print(f'Hola {otra_persona.nombre}, mi edad es: {self.edad}')
         #la línea de abajo se usa con python3.6 en adelante
         #return f'Hola {otra_persona.nombre}, mi nombre es: {self.nombre}.'
 
 messi = Persona("Lio", 34)
-cr = Persona("Cr", 37)
+cr = Persona("Ronaldo", 37)
 #invocando la función saluda
 messi.saluda(cr)
